9a076ad78293d3eaa801382d491297235b3d8302
Valentyna1402 <131916763+Valentyna1402@users.noreply.github.com>
2023-12-16 20:43:00 +0000

    Deploying to gh-pages from @ Valentyna1402/goit-react-hw-08-phonebook@1460d7ad44124381c70ee807357d498a09058143 🚀

---
+++ static/js/289.feeeb5dd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[289],{8289:function(e,t,r){r.r(t),r.d(t,{default:function(){return n}});var c=r(184);function n(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("h1",{children:"One place for all your contacts"}),(0,c.jsxs)("div",{children:[(0,c.jsx)("p",{children:"Manage contacts from a single place and access them everywhere."}),(0,c.jsx)("image",{src:"",alt:""})]}),(0,c.jsx)("button",{children:"Start"})]})}}}]);
+//# sourceMappingURL=289.feeeb5dd.chunk.js.map
